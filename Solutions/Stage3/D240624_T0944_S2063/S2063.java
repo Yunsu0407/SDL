f51d07505a7b4ee5e295c6801b72d97d06d52ed5
Yunsu0407 <aszx4949@naver.com>
2024-06-25 10:10:04 +0900

    complete 2063

--- Solutions/Stage3/D240624_T0944_S2063/S2063.java
+++ Solutions/Stage3/D240624_T0944_S2063/S2063.java
@@ -1,4 +1,67 @@
 package Solutions.Stage3.D240624_T0944_S2063;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class S2063 {
+    public static void main(String[] args) throws IOException {
+        findMidValue();
+    }
+
+    public static void findMidValue() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine().trim());
+        int[] array = new int[size];
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+
+        for (int i = 0; i < size; ++i) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+        mergeSort(array, 0, size - 1);
+        System.out.println(array[size / 2]);
+    }
+
+    public static void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            mergeSort(array, left, middle);
+            mergeSort(array, middle + 1, right);
+            merge(array, left, middle, right);
+        }
+    }
+
+    public static void merge(int[] array, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            leftArray[i] = array[left + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            rightArray[j] = array[middle + 1 + j];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] >= rightArray[j]) {
+                array[k++] = leftArray[i++];
+            } else {
+                array[k++] = rightArray[j++];
+            }
+        }
+
+        while (i < n1) {
+            array[k++] = leftArray[i++];
+        }
+        while (j < n2) {
+            array[k++] = rightArray[j++];
+        }
+    }
 }
