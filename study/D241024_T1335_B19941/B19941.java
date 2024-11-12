4b824998b1c715a5109fc17344beac7d71f91c35
yunsu0407 <aszx4949@naver.com>
2024-11-12 15:26:08 +0900

    commit for pull

--- study/D241024_T1335_B19941/B19941.java
+++ study/D241024_T1335_B19941/B19941.java
@@ -1,4 +1,20 @@
 package study.D241024_T1335_B19941;
 
+import java.io.*;
+import java.util.*;
+
 public class B19941 {
+    public static void main(String[] args) throws IOException {
+        assignFood();
+    }
+
+    public static void assignFood() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        final int SIZE = Integer.parseInt(st.nextToken());
+        final int RANGE = Integer.parseInt(st.nextToken());
+        String state = br.readLine().trim();
+
+
+    }
 }
---
+++ study/D241108_T1615_B9655/B9655.java
@@ -0,0 +1,20 @@
+package study.D241108_T1615_B9655;
+
+import java.util.*;
+import java.io.*;
+
+public class B9655 {
+    public static void main(String[] args)throws IOException{
+        stoneGame();
+    }
+
+    public static void stoneGame() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int stone = Integer.parseInt(br.readLine().trim());
+        int count = 0;
+    }
+
+    public static void 
+
+
+}
