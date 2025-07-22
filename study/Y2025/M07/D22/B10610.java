3738fe5b0baeedd3db5e01d7bc4c532fe0ea27a4
yunsu0407 <aszx4949@naver.com>
2025-07-22 15:35:41 +0900

    B10610, B1094

---
+++ study/Y2025/M07/D22/B10610.java
@@ -0,0 +1,41 @@
+package study.Y2025.M07.D22;
+
+import java.util.*;
+import java.io.*;
+
+public class B10610 {
+    public static void main(String[] args) throws IOException {
+        multiple30();
+    }
+
+    public static String input1 = "102"; // 210
+    public static String input2 = "2931"; // -1
+    public static String input3 = "80875542"; // 88755420
+
+    public static void multiple30() throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new StringReader(input3));
+        String str = br.readLine().trim();
+
+        int[] digit = new int[10];
+        int sum = 0;
+        for (int i = 0; i < str.length(); ++i) {
+            int value = str.charAt(i) - '0';
+            sum = sum + value;
+            ++digit[value];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (sum % 3 != 0 || digit[0] == 0) {
+            sb.append("-1");
+        } else {
+            for (int i = digit.length - 1; i >= 0; --i) {
+                for (int j = 0; j < digit[i]; ++j) {
+                    sb.append(i);
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
---
+++ study/Y2025/M07/D22/B1094.java
@@ -0,0 +1,31 @@
+package study.Y2025.M07.D22;
+
+import java.util.*;
+import java.io.*;
+
+public class B1094 {
+    public static void main(String[] args) throws IOException {
+        stick();
+    }
+
+    public static String input1 = "23"; // 4
+    public static String input2 = "32"; // 1
+    public static String input3 = "64"; // 1
+    public static String input4 = "48"; // 2
+
+    public static void stick() throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new StringReader(input4));
+        int value = Integer.parseInt(br.readLine().trim());
+
+        int count = 0;
+        while (value != 0) {
+            if (value % 2 != 0) {
+                ++count;
+            }
+            value = value / 2;
+        }
+
+        System.out.println(count);
+    }
+}
