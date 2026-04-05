3217328ecd77b2a293034444dac7fbf685017fbd
Yunsu0407 <aszx4949@naver.com>
2026-04-05 21:41:09 +0900

    B11651

---
+++ src/Y2026/M04/D05/B11651.java
@@ -0,0 +1,38 @@
+package D05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class B11651 {
+    public static void main(String[] args) throws IOException {
+        arrageCoord();
+    }
+
+    public static void arrageCoord() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine().trim());
+
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < size; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine().trim());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list.add(new int[] { x, y });
+        }
+
+        list.sort(
+                Comparator.comparingInt((int[] i) -> i[1])
+                        .thenComparingInt(i -> i[0]));
+
+        StringBuilder sb = new StringBuilder();
+        for (int[] curr : list) {
+            sb.append(curr[0]).append(" ").append(curr[1]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
