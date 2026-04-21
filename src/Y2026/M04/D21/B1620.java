f0d5049539a5eaf3d0a12697974708405042e8fc
yunsu0407 <aszx4949@naver.com>
2026-04-21 16:01:39 +0900

    B1620

--- src/Y2026/M04/D21/B1620.java
+++ src/Y2026/M04/D21/B1620.java
@@ -8,48 +8,13 @@ public class B1620 {
         makeDictionary();
     }
 
-    public static String input1 = "26 5\r\n" + //
-            "Bulbasaur\r\n" + //
-            "Ivysaur\r\n" + //
-            "Venusaur\r\n" + //
-            "Charmander\r\n" + //
-            "Charmeleon\r\n" + //
-            "Charizard\r\n" + //
-            "Squirtle\r\n" + //
-            "Wartortle\r\n" + //
-            "Blastoise\r\n" + //
-            "Caterpie\r\n" + //
-            "Metapod\r\n" + //
-            "Butterfree\r\n" + //
-            "Weedle\r\n" + //
-            "Kakuna\r\n" + //
-            "Beedrill\r\n" + //
-            "Pidgey\r\n" + //
-            "Pidgeotto\r\n" + //
-            "Pidgeot\r\n" + //
-            "Rattata\r\n" + //
-            "Raticate\r\n" + //
-            "Spearow\r\n" + //
-            "Fearow\r\n" + //
-            "Ekans\r\n" + //
-            "Arbok\r\n" + //
-            "Pikachu\r\n" + //
-            "Raichu\r\n" + //
-            "25\r\n" + //
-            "Raichu\r\n" + //
-            "3\r\n" + //
-            "Pidgey\r\n" + //
-            "Kakuna";
-
     public static void makeDictionary() throws IOException {
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader br = new BufferedReader(new StringReader(input1));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine().trim());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         Map<String, String> map = new HashMap<>();
-
         for (int i = 0; i < n; ++i) {
             String number = String.valueOf(i + 1);
             String name = br.readLine().trim();
