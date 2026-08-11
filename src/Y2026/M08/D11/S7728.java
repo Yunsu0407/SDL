8dda49e795f8e33e8338c8895457fbe4c9fecf7e
Yunsu0407 <aszx4949@naver.com>
2026-08-11 14:19:59 +0900

    S7728

--- src/Y2026/M08/D11/S7728.java
+++ src/Y2026/M08/D11/S7728.java
@@ -2,34 +2,36 @@ package Y2026.M08.D11;
 
 import java.io.*;
 
-public class S1289 {
+public class S7728 {
     public static void main(String[] args) throws IOException {
-        fixMemory();
+        numberDiversity();
     }
 
-    public static void fixMemory() throws IOException {
+    public static void numberDiversity() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testcase = Integer.parseInt(br.readLine().trim());
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < testcase; ++i) {
             String given = br.readLine().trim();
-            int prev = 0;
+            boolean[] isAppear = new boolean[10];
 
-            int count = 0;
             for (int j = 0; j < given.length(); ++j) {
-                int curr = given.charAt(j) - '0';
+                int digit = given.charAt(j) - '0';
+                isAppear[digit] = true;
+            }
 
-                if (prev != curr) {
-                    ++count;
-                    prev = curr;
+            int diversity = 0;
+            for (boolean b : isAppear) {
+                if (b) {
+                    ++diversity;
                 }
             }
 
-            String line = String.format("#%d %d\n", (i + 1), count);
+            String line = String.format("#%d %d\n", (i + 1), diversity);
             sb.append(line);
         }
 
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
     }
 }
