bd7b677c47bb1d6b74daf39aa8edf8c60ca2871d
Yunsu0407 <aszx4949@naver.com>
2026-08-12 11:12:28 +0900

    S6913

---
+++ src/Y2026/M08/D12/S6913.java
@@ -0,0 +1,45 @@
+package Y2026.M08.D12;
+
+import java.util.*;
+import java.io.*;
+
+public class S6913 {
+    public static void main(String[] args) throws IOException {
+        findFirst();
+    }
+
+    public static void findFirst() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine().trim());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < testcase; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine().trim());
+            int n = Integer.parseInt(st.nextToken()); // person
+            int m = Integer.parseInt(st.nextToken()); // problem
+
+            int max = 0;
+            int[] scores = new int[n];
+            for (int j = 0; j < n; ++j) {
+                st = new StringTokenizer(br.readLine().trim());
+
+                for (int k = 0; k < m; ++k) {
+                    scores[j] = scores[j] + Integer.parseInt(st.nextToken());
+                }
+                max = Math.max(max, scores[j]);
+            }
+
+            int count = 0;
+            for (int j = 0; j < n; ++j) {
+                if (max == scores[j]) {
+                    ++count;
+                }
+            }
+
+            String line = String.format("#%d %d %d\n", (i + 1), count, max);
+            sb.append(line);
+        }
+
+        System.out.print(sb.toString());
+    }
+}
